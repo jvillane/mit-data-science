7a5e5df9b0c636ac68fd70b06088fc078adfd209
jvillane <jvillane@gmail.com>
2019-05-08 16:29:55 -0400

    UPD: varios

---
+++ case-study-2-1/install.R
@@ -0,0 +1,11 @@
+install.packages("hdm")
+
+install.packages("hdm", repos="http://R-Forge.R-project.org")
+# binary, if your system is up to date
+install.packages("hdm", repos="http://R-Forge.R-project.org", type="source")
+# source code, if your system is not up to date
+
+library(hdm)
+
+
+install.packages("xtable")
